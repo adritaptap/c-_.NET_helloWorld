90e33b7f538aca1c4ebf2451ca177df2cfe3a6f0
adritaptap <adrien.juhem@campus-numerique-in-the-alps.com>
2017-08-21 17:16:02 +0200

    Ajoutez des fichiers projet.

---
+++ helloWorld2/Message.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld2
+{
+    class Message
+    {
+        private int hour = DateTime.Now.Hour;
+        private string day = DateTime.Now.DayOfWeek.ToString();
+        private string message;
+        private int minMorning;
+        private int maxMorning;
+        private int maxAfternoon;
+
+        public Message()
+        {
+            this.minMorning = 8;
+            this.maxMorning = 13;
+            this.maxAfternoon = 18;
+        }
+
+        public Message(int minM, int maxM, int maxA)
+        {
+            this.minMorning = minM;
+            this.maxMorning = maxM;
+            this.maxAfternoon = maxA;
+        }
+
+        public string HelloMessage
+        {
+            get
+            {
+                if ((day == "sunday") || (day == "saturday") || (day == "monday" && hour < minMorning) || (hour > maxAfternoon && day == "friday"))
+                {
+                    message = "Bon weekend";
+                }
+                else
+                {
+                    if (hour > minMorning && hour < maxMorning)
+                    {
+                        message = "Bonjour";
+
+                    }
+                    else if (hour >= maxMorning && hour <= maxAfternoon)
+                    {
+                        message = "Bon après-midi";
+                    }
+                    else
+                    {
+                        message = "Bonsoir";
+                    }
+
+
+                }
+                return message;
+            }
+        }
+    }
+}
---
+++ helloWorld2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new Message();
+            string line;
+            string[] user = Environment.UserName.Split('.');
+            string lastName = user[1].First() + String.Join("", user[1].Skip(1)).ToLower();
+           
+            do
+            {
+                Console.WriteLine(message.HelloMessage + " " + user[0] + " " + lastName);
+                Console.WriteLine("que voulez-vous faire ?");
+                line = Console.ReadLine().ToString();
+            } while (line != "exit");
+        }
+    }
+}
